37dd060dd268ae5936392a7c212c5acf29528aaa
jf-arce <josefranciscoarce21@gmail.com>
2025-02-08 02:08:22 -0300

    feat: cambia la forma de generar una api key

--- src/utils/generateApiKey.ts
+++ src/utils/generateApiKey.ts
@@ -1,3 +1,3 @@
 export const generateApiKey = () => {
-  return Math.random().toString(36).substring(2); // Genera una clave aleatoria
+  return Array.from({ length: 40 }, () => Math.random().toString(36).charAt(2)).join('');
 };
